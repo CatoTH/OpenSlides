50eec562afb658c43a3d34096a2bec8b0a3b07df
Tobias Hößl <tobias@hoessl.eu>
2016-08-07 17:22:31 +0200

    More work on line extraction

--- openslides/motions/static/js/motions/diff.js
+++ openslides/motions/static/js/motions/diff.js
@@ -2,17 +2,155 @@
 
 "use strict";
 
-angular.module('OpenSlidesApp.motions.diff', [])
+angular.module('OpenSlidesApp.motions.diff', ['OpenSlidesApp.motions.lineNumbering'])
 
 /**
  * Current limitations of this implementation:
  *
  */
 
-.service('lineNumberingService', function () {
+.service('diffService', function (lineNumberingService) {
+    var ELEMENT_NODE = 1,
+        TEXT_NODE = 3;
+
 
     this.getLineNumberNode = function(fragment, lineNumber) {
         return fragment.querySelector('span.os-line-number.line-number-' + lineNumber);
+    };
+
+    this._getNodeContextTrace = function(node) {
+        var context = [],
+            currNode = node;
+        while (currNode) {
+            context.unshift(currNode);
+            currNode = currNode.parentNode;
+        }
+        return context;
+    };
+
+  /*
+   * Returns an array with the following values:
+   * 0: the most specific DOM-node that contains both line numbers
+   * 1: the context of node1 (an array of dom-elements; 0 is the document fragment)
+   * 2: the context of node2 (an array of dom-elements; 0 is the document fragment)
+   * 3: the index of [0] in the two arrays
+   */
+    this._getCommonAncestor = function(node1, node2) {
+        var trace1 = this._getNodeContextTrace(node1),
+            trace2 = this._getNodeContextTrace(node2),
+            commonAncestor = null,
+            commonIndex = null,
+            childTrace1 = [],
+            childTrace2 = [];
+
+        for (var i = 0; i < trace1.length && i < trace2.length; i++) {
+            if (trace1[i] == trace2[i]) {
+                commonAncestor = trace1[i];
+                commonIndex = i;
+            }
+        }
+        for (i = commonIndex + 1; i < trace1.length; i++) {
+            childTrace1.push(trace1[i]);
+        }
+        for (i = commonIndex + 1; i < trace2.length; i++) {
+            childTrace2.push(trace2[i]);
+        }
+        return {
+            'commonAncestor': commonAncestor,
+            'trace1' : childTrace1,
+            'trace2' : childTrace2,
+            'index': commonIndex
+        };
+    };
+
+    this._serializeTag = function(node) {
+        var html = '<' + node.nodeName;
+        for (var i = 0; i < node.attributes.length; i++) {
+          var attr = node.attributes[i];
+          html += " " + attr.name + "=\"" + attr.value + "\"";
+        }
+        html += '>';
+        return html;
+    };
+
+    this._serializeDom = function(node) {
+        if (node.nodeName == 'BR') {
+            return '<BR>';
+        } else {
+            var html = this._serializeTag(node);
+            for (var i = 0; i < node.childNodes.length; i++) {
+                if (node.childNodes[i].nodeType == TEXT_NODE) {
+                    html += node.childNodes[i].nodeValue;
+                } else if (
+                  !lineNumberingService._isOsLineNumberNode(node.childNodes[i]) &&
+                  !lineNumberingService._isOsLineBreakNode(node.childNodes[i])
+                ) {
+                    html += this._serializeDom(node.childNodes[i]);
+                }
+            }
+            html += '</' + node.nodeName + '>';
+
+            return html;
+        }
+    };
+
+    this._serializePartialDomToChild = function(node, toChildTrace) {
+        var html = '<' + node.nodeName + '>';
+        for (var i = 0, found = false; i < node.childNodes.length && !found; i++) {
+            if (node.childNodes[i] == toChildTrace[0]) {
+                found = true;
+                var remainingTrace = toChildTrace;
+                remainingTrace.shift();
+                if (!lineNumberingService._isOsLineNumberNode(node.childNodes[i])) {
+                    html += this._serializePartialDomToChild(node.childNodes[i], remainingTrace);
+                }
+            } else if (node.childNodes[i].nodeType == TEXT_NODE) {
+                html += node.childNodes[i].nodeValue;
+            } else {
+                if (
+                  !lineNumberingService._isOsLineNumberNode(node.childNodes[i]) &&
+                  !lineNumberingService._isOsLineBreakNode(node.childNodes[i])
+                ) {
+                    html += this._serializeDom(node.childNodes[i]);
+                }
+            }
+        }
+        return html;
+    };
+
+    this._serializePartialDomFromChild = function(node, fromChildTrace) {
+        var html = '';
+        for (var i = 0, found = false; i < node.childNodes.length; i++) {
+            if (node.childNodes[i] == fromChildTrace[0]) {
+                found = true;
+                var remainingTrace = fromChildTrace;
+                remainingTrace.shift();
+                if (!lineNumberingService._isOsLineNumberNode(node.childNodes[i])) {
+                    html += this._serializePartialDomFromChild(node.childNodes[i], remainingTrace);
+                }
+            } else if (found) {
+                if (node.childNodes[i].nodeType == TEXT_NODE) {
+                    html += node.childNodes[i].nodeValue;
+                } else {
+                    if (
+                      !lineNumberingService._isOsLineNumberNode(node.childNodes[i]) &&
+                      !lineNumberingService._isOsLineBreakNode(node.childNodes[i])
+                    ) {
+                        html += this._serializeDom(node.childNodes[i]);
+                    }
+                }
+            }
+        }
+        html += '</' + node.nodeName + '>';
+        return html;
+    };
+
+    this.extractRangeByLineNumbers = function(fragment, fromLine, toLine) {
+        var fromLineNode = this.getLineNumberNode(fragment, fromLine),
+            toLineNode = this.getLineNumberNode(fragment, toLine),
+            ancestors = this._getCommonAncestor(fromLineNode, toLineNode);
+
+        return ;
     };
 });
 
--- tests/karma/motions/diff.service.test.js
+++ tests/karma/motions/diff.service.test.js
@@ -2,7 +2,7 @@ describe('linenumbering', function () {
 
   beforeEach(module('OpenSlidesApp.motions.diff'));
 
-  var lineNumberingService,
+  var diffService,
       brMarkup = function (no) {
         return '<br class="os-line-break">' +
             '<span class="os-line-number line-number-' + no + '" data-line-number="' + no + '" contenteditable="false">&nbsp;</span>';
@@ -11,8 +11,8 @@ describe('linenumbering', function () {
         return '<span class="os-line-number line-number-' + no + '" data-line-number="' + no + '" contenteditable="false">&nbsp;</span>';
       };
 
-  beforeEach(inject(function (_lineNumberingService_) {
-    lineNumberingService = _lineNumberingService_;
+  beforeEach(inject(function (_diffService_) {
+    diffService = _diffService_;
   }));
 
   describe('extraction of lines', function () {
@@ -23,9 +23,9 @@ describe('linenumbering', function () {
     holderDiv.innerHTML = '<div>' +
           noMarkup(1) + 'Line 1' + brMarkup(2) + 'Line 2' +
           brMarkup(3) + 'Line <strong>3' + brMarkup(4) + 'Line 4' + brMarkup(5) + 'Line</strong> 5' +
-          '<ul>' +
-            '<li>' + noMarkup(6) + 'Line 6' + brMarkup(7) + 'Line 7' + '</li>' +
-            '<li><ul>' +
+          '<ul class="ul-class">' +
+            '<li class="li-class">' + noMarkup(6) + 'Line 6 ' + brMarkup(7) + 'Line 7' + '</li>' +
+            '<li class="li-class"><ul>' +
               '<li>' + noMarkup(8) + 'Level 2 LI 8</li>' +
               '<li>' + noMarkup(9) + 'Level 2 LI 9</li>' +
             '</ul></li>' +
@@ -33,18 +33,58 @@ describe('linenumbering', function () {
           noMarkup(10) + 'Line 10' + brMarkup(11) + 'Line 11</div>';
 
     it('locates line number nodes', function() {
-      var lineNumberNode = lineNumberingService.getLineNumberNode(baseHtmlDom, 4);
+      var lineNumberNode = diffService.getLineNumberNode(baseHtmlDom, 4);
       expect(lineNumberNode.parentNode.nodeName).toBe('STRONG');
 
-      lineNumberNode = lineNumberingService.getLineNumberNode(baseHtmlDom, 9);
+      lineNumberNode = diffService.getLineNumberNode(baseHtmlDom, 9);
       expect(lineNumberNode.parentNode.nodeName).toBe('LI');
 
-      lineNumberNode = lineNumberingService.getLineNumberNode(baseHtmlDom, 15);
+      lineNumberNode = diffService.getLineNumberNode(baseHtmlDom, 15);
       expect(lineNumberNode).toBe(null);
     });
 
-    it('extracts a single line', function () {
+    it('finds the common ancestor', function() {
+      var fromLineNode, toLineNode, commonAncestor;
+
+      fromLineNode = diffService.getLineNumberNode(baseHtmlDom, 6);
+      toLineNode = diffService.getLineNumberNode(baseHtmlDom, 7);
+      commonAncestor = diffService._getCommonAncestor(fromLineNode, toLineNode);
+      expect(commonAncestor.commonAncestor.nodeName).toBe("LI");
+
+      fromLineNode = diffService.getLineNumberNode(baseHtmlDom, 6);
+      toLineNode = diffService.getLineNumberNode(baseHtmlDom, 8);
+      commonAncestor = diffService._getCommonAncestor(fromLineNode, toLineNode);
+      expect(commonAncestor.commonAncestor.nodeName).toBe("UL");
+
+      fromLineNode = diffService.getLineNumberNode(baseHtmlDom, 6);
+      toLineNode = diffService.getLineNumberNode(baseHtmlDom, 10);
+      commonAncestor = diffService._getCommonAncestor(fromLineNode, toLineNode);
+      expect(commonAncestor.commonAncestor.nodeName).toBe("DIV");
 
     });
+
+    it('renders DOMs correctly', function() {
+      var lineNo = diffService.getLineNumberNode(baseHtmlDom, 7),
+          greatParent = lineNo.parentNode.parentNode,
+          lineTrace = [lineNo.parentNode, lineNo];
+
+      var pre = diffService._serializePartialDomToChild(greatParent, lineTrace);
+      expect(pre).toBe('<UL><LI>Line 6 ');
+
+      lineTrace = [lineNo.parentNode, lineNo];
+      var post = diffService._serializePartialDomFromChild(greatParent, lineTrace);
+      expect(post).toBe('Line 7' + '</LI>' +
+            '<LI class="li-class"><UL>' +
+              '<LI>Level 2 LI 8</LI>' +
+              '<LI>Level 2 LI 9</LI>' +
+            '</UL></LI>' +
+          '</UL>');
+    });
+    /*
+    it('extracts a single line', function () {
+      var diff = diffService.extractRangeByLineNumbers(baseHtmlDom, 1, 2);
+      expect(diff).toBe('Test1');
+    });
+    */
   });
 });
